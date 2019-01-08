a2e559f058604428dc050153581e97c962bcb6e4
JH <snvkdnsem@naver.com>
2019-01-08 16:38:45 +0900

    11

---
+++ GenericTest/GenericTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GenericTest
+{
+    //일반화 클래스
+    public class GTest<T>
+    {
+        internal void Print(T i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    //일반화 메소드
+    public class GTest2
+    {
+        internal void Print<T>(T i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GTest<int> p1 = new GTest<int>();
+            p1.Print(9999);
+            GTest<string> p2 = new GTest<string>();
+            p2.Print("홍길동");
+
+            GTest2 g2 = new GTest2();
+            g2.Print<int>(1111);
+            g2.Print<string>("홍길동");
+        }
+    }
+}

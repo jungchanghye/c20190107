eea1204ea195dac967d2f124b7f04016eb96f854
JH <snvkdnsem@naver.com>
2019-01-09 13:27:04 +0900

    11

---
+++ Conditional/Conditional/Program.cs
@@ -0,0 +1,20 @@
+﻿#define SOUNDCARD // 이 부분을 지운 후 실행 해 보자
+#define SPEAKER
+
+using System;
+using System.Diagnostics;
+
+class Test
+{
+    [Conditional("SOUNDCARD")]
+    static void isSound() { isSpeaker(); }
+    [Conditional("SPEAKER")]
+    static void isSpeaker()
+    {
+        Console.WriteLine("음악을 들을 수 있습니다...");
+    }
+    static void Main()
+    {
+        isSound();
+    }
+}
---
+++ DllImpoertTest/DllImpoertTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DllImpoertTest
+{
+    class DllImpoertTest
+    {
+        [DllImport("User32.Dll")]
+        public static extern int MessageBox(int h, string m, string c, int type);
+
+        static void Main(string[] args)
+        {
+            MessageBox(0, "Hello!", "In C#", 0);
+        }
+    }
+}

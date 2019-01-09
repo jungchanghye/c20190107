0e87f09f9653838c45f96832e34acae4c619927a
JH <snvkdnsem@naver.com>
2019-01-09 11:03:07 +0900

    11

---
+++ IndexerTest/IndexerTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerTest
+{
+    class Emp
+    {
+        private string[] hobbies = new string[3];
+        public string this[int index] //인덱서
+        {
+            get
+            {
+                return hobbies[index];
+            }
+            set
+            {
+                hobbies[index] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();
+            e[0] = "등산";
+            e[1] = "낚시";
+            e[2] = "골프";
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{e[i]}");
+            }
+        }
+    }
+}
---
+++ OverLoadTest/OverLoadTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OverLoadTest
+{
+    class Emp
+    {
+        public int sal;
+        public static bool operator >= (Emp e1, Emp e2)
+        {
+            return (e1.sal >= e2.sal);
+        }
+        public static bool operator <= (Emp e1, Emp e2)
+        {
+            return (e1.sal <= e2.sal);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e1 = new Emp(); e1.sal = 1000;
+            Emp e2 = new Emp(); e2.sal = 2000;
+            if (e1 >= e2)
+            {
+                Console.WriteLine("e1이 크다 {0} >= {1}", e1.sal, e2.sal);
+            }
+            else
+            {
+                Console.WriteLine("e2가 크다 {0} < {1}", e1.sal, e2.sal);
+            }
+        }
+    }
+}

bf2e742cae0c50ca35eca4a6c559ff3a0d7d71f0
JH <snvkdnsem@naver.com>
2019-01-10 10:44:17 +0900

    11

---
+++ ThreadTest3/ThreadTest3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest3
+{
+    class ThreadTest3
+    {
+        public bool sleep = false;
+        public void FirstWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("F{0}", i);
+                if (i == 5)
+                {
+                    sleep = true;
+                    Console.WriteLine("");
+                    Console.WriteLine("first 쉼...");
+                    Thread.CurrentThread.Suspend();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadTest3 t = new ThreadTest3();
+            Thread first = new Thread(t.FirstWork);
+            first.Start();
+            while (t.sleep == false) { }
+            Console.WriteLine("first를 깨움니다.. 2초후 깨어 납니다.");
+            Thread.Sleep(2000);
+            first.Resume();
+            
+        }
+    }
+}
---
+++ ThreadTest4/ThreadTest4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static int mysum = 0;
+        static void Sum(object n)
+        {
+            int sum = 0;
+            int[] number = (int[])n;
+            for (int i = number[0]; i <= number[1]; i++)
+            {
+                sum += i;
+            }
+            mysum += sum;
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t2 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t3 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t4 = new Thread(new ParameterizedThreadStart(Sum));
+            Thread t5 = new Thread(new ParameterizedThreadStart(Sum));
+
+            t1.Start(new int[] { 1, 20 }); t2.Start(new int[] { 21, 40 });
+            t3.Start(new int[] { 41,60 }); t4.Start(new int[] { 61, 80 });
+            t5.Start(new int[] { 81, 100 });
+            t1.Join(); t2.Join(); t3.Join(); t4.Join(); t5.Join();
+            Console.WriteLine("1부터00까지의 합은 :: {0}", mysum);
+        }
+    }
+}

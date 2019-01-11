92e3a096ec216826119a5cdf810f68f9c13e984f
Mikyung Jeon <mikyung22e@naver.com>
2019-01-11 17:22:20 +0900

    11

---
+++ OracleAdapterDSTest2/OracleAdapterDSTest2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+class Example
+{
+    public static void Main()
+    {
+        string str = "data source=topcredu;User ID = scott; Password = tiger";
+        str = @"Data Source=(DESCRIPTION =
+                        (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.27)(PORT = 1521))
+                            (CONNECT_DATA =
+                                (SERVER = DEDICATED)
+                                (SERVICE_NAME = topcredu)
+                            )
+                        ) ;User Id=scott;Password=tiger";
+        OracleConnection Conn = new OracleConnection(str);
+        //Adapter 생성
+        OracleDataAdapter adapter = new OracleDataAdapter("select * from emp", Conn);
+        //CommandBuilder 개체를 이용하여 SQL 명령을 만든다.
+        OracleCommandBuilder builder = new OracleCommandBuilder(adapter);
+        //관련된 데이터 테이블들, 행, 열을 담을 DataSet 개체를 만든다.
+        DataSet ds = new DataSet();
+        //DataAdapter 를 이용하여 DataSet 에 “사원”이라는 이름으로 데이터를 채운다.
+        adapter.Fill(ds, "사원");
+        //데이터 건수를 화면에 출력 하자.
+        Console.WriteLine(" Data Count : {0}", ds.Tables["사원"].Rows.Count);
+        //DataCulumn 배열을 이용하여 기본키를 설정 한다
+        //Find 를 사용하기 위해 기본키를 설정, 키를 설정 할려면 컬럼의
+        //배열에 담아야 한다. 그런다음 그 배열을 PrimaryKey 속성에 배정한다.
+        DataColumn[] keys = new DataColumn[1];
+        keys[0] = ds.Tables["사원"].Columns["empno"];
+        ds.Tables["사원"].PrimaryKey = keys;
+        /* 위의것과 또 다른 방법으로 데이터베이스로 부터 직접 기본키 정보를
+        얻을 수 있는데 이 경우엔 DataSet 을 채우기 전에 명시적으로 키
+        정보를 빼먹지 말고 가지고 오라고 해야 한다.
+        * adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+        * adapter.Fill(ds, "사원")
+        * 이렇게 하면 DataTable 의 PrimaryKey 속성을 직접 설정 한 것과 동일한
+        효과를 얻는다
+        */
+        //Find 는 키값으로 DataRow 객체를 반환한다. 못찾으면 null 을 돌려준다.
+        DataRow findrow = ds.Tables["사원"].Rows.Find(7369);
+        if (findrow != null)
+        {
+            findrow.Delete();
+            adapter.Update(ds, "사원");
+            //삭제 후 데이터 출력
+            foreach (DataRow row in ds.Tables["사원"].Rows)
+            {
+                foreach (DataColumn col in ds.Tables["사원"].Columns)
+                    Console.Write("{0, -10}", row[col.ColumnName]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OracleAdapterDSTest3/OracleAdapterDSTest3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+class Sample
+{
+    public static void Main()
+    {
+        string str = @"Data Source=(DESCRIPTION =
+                        (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.27)(PORT = 1521))
+                            (CONNECT_DATA =
+                                (SERVER = DEDICATED)
+                                (SERVICE_NAME = topcredu)
+                            )
+                        ) ;User Id=scott;Password=tiger";
+        OracleConnection thisConnection = new OracleConnection(str);
+        thisConnection.Open();
+        OracleDataAdapter thisAdapter = new
+        OracleDataAdapter("SELECT empno, ename from emp", thisConnection);
+
+        OracleCommandBuilder thisBuilder = new OracleCommandBuilder(thisAdapter);
+        Console.WriteLine("SQL SELECT Command is:\n{ 0}\n", thisAdapter.SelectCommand.CommandText);
+        OracleCommand updateCommand = thisBuilder.GetUpdateCommand();
+        Console.WriteLine("SQL UPDATE Command is:\n{0}\n", updateCommand.CommandText);
+        OracleCommand insertCommand = thisBuilder.GetInsertCommand();
+        Console.WriteLine("SQL INSERT Command is:\n{0}\n",
+        insertCommand.CommandText);
+        OracleCommand deleteCommand = thisBuilder.GetDeleteCommand();
+        Console.WriteLine("SQL DELETE Command is:\n{0}",
+       deleteCommand.CommandText);
+        thisConnection.Close();
+    }
+}
